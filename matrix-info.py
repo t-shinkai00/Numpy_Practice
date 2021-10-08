c0d50c7ca50448089b327dd8aea5a6f36b2f1df6
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-08 11:12:34 +0900

    add

---
+++ matrix-info.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+a=np.array([2,3,4])
+b=np.array([
+  [1.2, 3.5, 5.1],
+  [-0.3, 1.1, -4.5]
+])
+
+# 形状
+print(a.shape)
+print(b.shape)
+
+# 次元
+print(a.ndim)
+print(b.ndim)
+
+# データ型
+print(a.dtype)
+print(b.dtype)
+
+# 要素数
+print(a.size)
+print(b.size)
