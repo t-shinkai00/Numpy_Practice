d84a1327797c30bb95c7885c457ef791b0e6bee4
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-08 14:57:38 +0900

    add applied-calc

--- calc-matrix.py
+++ calc-matrix.py
@@ -20,3 +20,19 @@ print(a@b)
 
 # アダマール積
 print(a*b)
+
+# 転置
+print(a.T)
+print(b.T)
+
+# 行列式
+print(np.linalg.det(a))
+print(np.linalg.det(b))
+
+# 逆行列
+print(np.linalg.inv(a))
+print(np.linalg.inv(b))
+
+# 行列×スカラー
+print(a*3)
+print(b*2)
